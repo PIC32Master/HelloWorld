6ef684ee7b5183369320c8069d4a09042a14a680
PIC32Master <champ@microchip.com>
2015-10-26 13:35:48 -0500

    Initial Commit to learn GIT by working with this project

---
+++ GitTest.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=GitTest.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/GitTest.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=gittest.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/gittest.x.tar
